3ae2400e1a0dbc116af03097201310e5f9c43a9d
JavierCondeCortes <jccfmys@gmail.com>
2026-01-08 18:03:34 +0100

    correcion de errores en texto principal

--- app/components/principalWeb/elementoTexto/text.tsx
+++ app/components/principalWeb/elementoTexto/text.tsx
@@ -1,27 +1,32 @@
-'use server'
+type TextoProps = {
+    titulo: string;
+    texto: string;
+    imagen: string;
+    orientacion: 'izq' | 'der';
+};
 
-export async function Texto({ titulo, texto, imagen, orientacion }) {
+export default function Texto({ titulo, texto, imagen, orientacion }: TextoProps) {
     let contenido;
+
     switch (orientacion) {
         case 'izq':
             contenido = (
                 <div className="flex flex-col md:flex-row items-center md:justify-center gap-6 md:gap-12 px-6 md:px-0 py-12 md:py-10 bg-[var(--darkCandemor)]">
                     <div>
                         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-white">{titulo}</h2>
-                        <p className="text-lg md:text-xl text-gray-300 max-w-md">
-                            {texto}
-                        </p>
+                        <p className="text-lg md:text-xl text-gray-300 max-w-md">{texto}</p>
                     </div>
                     <div className="w-full md:w-1/2 flex justify-center md:justify-center">
                         <img
                             src={`/img/${imagen}`}
                             alt="Sobre Nosotros"
-                            className="w-full h-auto max-w-sm rounded-lg "
+                            className="w-full h-auto max-w-sm rounded-lg"
                         />
                     </div>
                 </div>
             );
             break;
+
         case 'der':
         default:
             contenido = (
@@ -30,22 +35,17 @@ export async function Texto({ titulo, texto, imagen, orientacion }) {
                         <img
                             src={`/img/${imagen}`}
                             alt="Sobre Nosotros"
-                            className="w-full h-auto max-w-sm rounded-lg "
+                            className="w-full h-auto max-w-sm rounded-lg"
                         />
                     </div>
                     <div>
                         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-white">{titulo}</h2>
-                        <p className="text-lg md:text-xl text-gray-300 max-w-md">
-                            {texto}
-                        </p>
+                        <p className="text-lg md:text-xl text-gray-300 max-w-md">{texto}</p>
                     </div>
                 </div>
             );
             break;
     }
-    return (
-        <div>
-            {contenido}
-        </div>
-    );
+
+    return <div>{contenido}</div>;
 }
